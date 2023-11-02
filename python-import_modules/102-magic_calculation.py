e0b63edb5345fda0adb4f00af32fbc5e765ce780
nmaketh <n.riak@alustudent.com>
2023-11-02 21:21:34 +0200

    102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
-
-
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
