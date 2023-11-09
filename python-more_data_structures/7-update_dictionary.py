2c63f2ef02c021a4b500b0a0a65601721345b20c
nmaketh <n.riak@alustudent.com>
2023-11-09 18:18:54 +0200

    7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# 7-update_dictionary.py
 
 
 def update_dictionary(a_dictionary, key, value):
-    """Replace or add key/value pairs in a dictionary."""
+    """
+    A function that replaces or adds
+    key/value in a dictionary.
+    """
     a_dictionary[key] = value
-    return (a_dictionary)
+    return a_dictionary
