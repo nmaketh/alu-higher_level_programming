83e5671c334159a105c8e9cff1fa9dbc721a0d2c
nmaketh <n.riak@alustudent.com>
2023-11-10 21:58:19 +0200

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return lambda my_list : my_list * number
-mymutiply =  mutiply_list_map(my_list, number)
-print(mymutiply)
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda n: n * number, my_list))
