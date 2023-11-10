d55a91af9b611dcedb31b59d684d9ff94ed90da4
nmaketh <n.riak@alustudent.com>
2023-11-10 11:06:46 +0200

    11-multiply_list_map.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
     return lambda my_list : my_list * number
-my_list = [1, 2, 3, 4, 6]
-mymutiply =  mutiply_list_map(my_list, 4)
-print(mymutiply(4))
+mymutiply =  mutiply_list_map(my_list, number)
+print(mymutiply)
