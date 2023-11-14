20adbdfe1e389153ea57d65071435a4837ca4e9d
nmaketh <n.riak@alustudent.com>
2023-11-14 14:11:43 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 
+""" write an empty  square"""
 class square:
     pass
-    """python3 -c 'print(__import__("my_module").__doc__)'
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-
-"""
-    def __init__(self):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
-         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-         
-         """
+    """empty square"""
