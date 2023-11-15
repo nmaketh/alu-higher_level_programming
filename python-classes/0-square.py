c2a0fef4c83e203ea7aeac1c9618f62c6ef11698
nmaketh <n.riak@alustudent.com>
2023-11-15 11:19:55 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,11 @@
 
 #!/usr/bin/python3
+"""Defines a class square:"""
 
 
 class Square:
+    """represent Square."""
     pass
+my_square = Square()
+print(type(my_square))
+print(my_isquare.__dict__)
