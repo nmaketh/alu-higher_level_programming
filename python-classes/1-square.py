06a6bd59e40ca534c674d2d96a9154421fe6121b
nmaketh <n.riak@alustudent.com>
2023-11-15 11:57:41 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 """Defines Square based on 0-square.py file"""
 class Square:
-    """represent the square."""
-    def __init__(self, size):
-       self.__size=size
+     """represent the square."""
+     def __init__(self, size):
+         self.__size = size
