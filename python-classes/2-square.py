1fc709fbbe827000422ed4fe0ddb0f035da7641e
nmaketh <n.riak@alustudent.com>
2023-11-16 02:42:57 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by: (based on 0-square.py)"""
+# 2-square.py
+"""Define a class Square."""
 
 
 class Square:
-    """Private instance attribute: size"""
-    
-    def __init___(self, size=0):
-       """ initializes size.
-       arg:
+    """Represent a square."""
 
-           size(int): the size of the new square.
-           """
-       if size != int:
-         raise TypeError("size must be an integer")
-       elif size < 0:
-          raise ValueError("size must be >= 0")
-       self.__size = size
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
