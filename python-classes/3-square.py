0ca6a1289ce721f2376ed7fe77612960050cd947
nmaketh <n.riak@alustudent.com>
2023-11-16 07:40:46 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,9 +13,12 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
+            
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """ returning the area of square"""
+        return {}.format(self.___size * 4)
         
