5da81c49e66d6e12d04d045729905bec9c6cde3e
nmaketh <n.riak@alustudent.com>
2023-11-16 02:39:44 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,15 @@
 
 class Square:
     """Private instance attribute: size"""
-    pass
+    
     def __init___(self, size=0):
-       self.__size = size
-       if self.__size != int:
+       """ initializes size.
+       arg:
+
+           size(int): the size of the new square.
+           """
+       if size != int:
          raise TypeError("size must be an integer")
-       elif self.__size < 0:
+       elif size < 0:
           raise ValueError("size must be >= 0")
+       self.__size = size
