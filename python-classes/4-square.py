3a393bd4def26429f040faefa7b3a089c6c23a34
nmaketh <n.riak@alustudent.com>
2023-11-17 05:41:36 -0800

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 3-square.py)
+
+Private instance attribute: size:
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+size must be an integer, otherwise raise a TypeError exception with the message size must
+"""
+class Square:
+    """ defines the class square"""
+    def ___init___(self, size=0):
+        """initialise the square"""
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size 
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size <0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return pow(self.__size, 2)
+
