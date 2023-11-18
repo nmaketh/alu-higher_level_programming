ca7a9d0850dae070077fcf43b625d953014f7152
nmaketh <n.riak@alustudent.com>
2023-11-18 06:49:20 -0800

    2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -43,9 +43,9 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width*self.__height
+        return self.__width * self.__height
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
            return 0
-        return 2*(self.__width + sef.__height)
+        return 2 *(self.__width + self.__height)
