362aa5bd5a0c38aff0873c412cb9802d592ebbec
nmaketh <n.riak@alustudent.com>
2023-11-18 00:25:28 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,21 +6,24 @@ property def size(self): to retrieve it
 property setter def size(self, value): to set it:
 size must be an integer, otherwise raise a TypeError exception with the message size must
 """
+
+
 class Square:
     """ defines the class square"""
     def ___init___(self, size=0):
         """initialise the square"""
         self.__size = size
+    def area(self):
+        return pow(self.__size, 2)
     @property
     def size(self):
+        """getting method """
         return self.__size 
     @size.setter
     def size(self, value):
+        """setting method"""
         if isinstance(type(value), int):
             raise TypeError("size must be an integer")
-        elif value <0:
+        if value <0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    def area(self):
-        return self.__size ** 2
-
