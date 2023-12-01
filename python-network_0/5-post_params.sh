17b243875d8021665c6f36283c4eb52bbb40fd2f
nmaketh <n.riak@alustudent.com>
2023-12-01 10:05:22 -0800

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash scripts that sends a POST request to a given URL.
-curl -sX POST -d email=hr@holbertonschool.com&subject="I will always be here for PLD" "$1"`
+curl -sX POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
